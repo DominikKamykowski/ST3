0f69f78279143b93c59f73d732ceb770bdc6a235
Dominik Kamykowski <kamyk054@op.pl>
2023-03-26 16:48:25 +0200

    Dtos class modify

--- SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/UpdateUserDto.cs
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/UpdateUserDto.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.dtos
 {
-    class UpdateUserDto
+    public class UpdateUserDto :IValidatableObject
     {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(PhoneNumber))
+            {
+                yield return new ValidationResult("Phone number or Email is required!",
+                    new[] { nameof(Email), nameof(PhoneNumber) });
+            }
+        }
     }
 }
--- SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/UserDto.cs
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/UserDto.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.dtos
 {
-    class UserDto
+    public record UserDto
     {
+        public Guid Id { set; get; }
+
+        [MaxLength(128)]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [MinLength(2)]
+        [MaxLength(64)]
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [MinLength(2)]
+        [MaxLength(64)]
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [MinLength(9)]
+        [MaxLength(9)]
+        public string PhoneNumber { get; set; }
+
     }
 }

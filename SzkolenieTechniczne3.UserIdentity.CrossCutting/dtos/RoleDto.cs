a505326b107632b4609a70f9bd94c9e223dbff4f
Dominik Kamykowski <kamyk054@op.pl>
2023-03-26 16:40:36 +0200

    New dtos project and base class

---
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/RoleDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.dtos
+{
+    public record RoleDto
+    {
+        public Guid Id { set; get; }
+
+        [MinLength(2)]
+        [MaxLength(64)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(64)]
+        public string Description { get; set; } = null!;
+        public Guid[]? UserIds { get; set; }
+    }
+}
---
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.dtos
+{
+    class UpdateUserDto
+    {
+    }
+}

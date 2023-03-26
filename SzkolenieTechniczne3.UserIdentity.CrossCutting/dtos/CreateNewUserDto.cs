c91f363152d29d88e323391470a0285796a25118
Dominik Kamykowski <kamyk054@op.pl>
2023-03-26 16:52:22 +0200

    CreateNewUserDto class create

--- SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/CreateNewUserDto.cs
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/dtos/CreateNewUserDto.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.dtos
 {
-    class CreateNewUserDto
+    class CreateNewUserDto :UpdateUserDto
     {
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(PhoneNumber))
+            {
+                yield return new ValidationResult("Phone number or Email is required!",
+                    new[] { nameof(Email), nameof(PhoneNumber) });
+            }
+            if(!string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(Password))
+            {
+                yield return new ValidationResult("Password is required", new[] { nameof(Password) });
+            }
+        }
     }
 }
